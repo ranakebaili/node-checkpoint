f3f9b5a05b1ff47ab95ad023518c5c1cae81ca93
ranakebaili <rana.kebaili@gmail.com>
2023-11-24 19:38:59 +0100

    jnkj

---
+++ Task2.js
@@ -0,0 +1,8 @@
+const http = require("http");
+
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h1>Hello Node!!!!</h1>");
+}).listen(3000, () => {
+  console.log("Server running at http://localhost:3000/");
+});
---
+++ Task3.js
@@ -0,0 +1,8 @@
+const fs = require('node:fs');
+fs.readFile('./welcome.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
